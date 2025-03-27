b2d51530ae147fd751d4097118f7bb0448a7db78
Otavio Neves <otaviomatheusneves@gmail.com>
2025-03-26 21:48:21 -0300

    versão 1

--- Aula 1 - POO/aula1.js
+++ Aula 1 - POO/aula1.js
@@ -10,7 +10,7 @@ class Cliente {
 let cliente = new Cliente();
 
 cliente.nome = prompt("Digite o nome do cliente: ");
-cliente.idade = 35;
+cliente.idade = 20;
 cliente.cpf = "999.999.999-01"
 cliente.agencia = 101010;
 
