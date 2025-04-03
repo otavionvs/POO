293e13b542c465cacc1ce724153debdd7193ca21
Otavio Neves <otaviomatheusneves@gmail.com>
2025-04-02 21:44:18 -0300

    Encapsulamento

--- Aula 1 - POO/aula1.js
+++ Aula 1 - POO/aula1.js
@@ -2,19 +2,35 @@ const prompt = require('prompt-sync')();
 
 // Modelo de atributos e funções para criação de um objeto
 class Cliente {
-    nome; //Atributo
+    // # -> priva o dado
+    #nome; //Atributo
     idade;
     cpf;
     agencia;
+    
+    // Retorna uma string com os dados/atributos
+    getDescricao() {
+        return "Nome: " + this.#nome + ", Idade: " + this.idade;
+    }
+
+    // Obtem nome
+    getNome() {
+        return this.#nome;
+    }
+
+    // Preenche nome
+    setNome(value) {
+        this.#nome = value;
+    }
 }
 
 // Objeto que possui as informações da classe de forma individual
 let cliente = new Cliente();
 
-cliente.nome = prompt("Digite o nome do cliente: ");
+cliente.setNome(prompt("Digite o nome do cliente: "));
 cliente.idade = 20;
 cliente.cpf = "999.999.999-01"
 cliente.agencia = 101010;
 
-console.log(cliente);
+console.log(cliente.getDescricao());
 
